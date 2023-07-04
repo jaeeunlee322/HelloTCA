2dc79c8a248afab279b0dd3f003bb3507fade286
jaeeunlee <jaeeun.lee@wesang.com>
2023-07-04 23:15:26 +0900

    [Essential: Your First Feature 1-1] 뷰 생성

---
+++ HelloTCA/CounterView.swift
@@ -0,0 +1,58 @@
+//
+//  CounterView.swift
+//  HelloTCA
+//
+//  Created by 이재은(Lee, Jaeeun) on 2023/07/04.
+//
+
+import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct CounterView: View {
+
+    let store: StoreOf<CounterFeature>
+
+    var body: some View {
+        WithViewStore(self.store,
+                      observe: { $0 }) { viewStore in
+            VStack {
+                Text("\(viewStore.count)")
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                HStack {
+                    Button("-") {
+                        viewStore.send(.decrementButtonTapped)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+
+                    Button("+") {
+                        viewStore.send(.incrementButtonTapped)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                }
+            }
+
+
+        }
+
+    }
+}
+
+struct CounterPreview: PreviewProvider {
+    static var previews: some View {
+        CounterView(
+            store: Store(initialState: CounterFeature.State()) {
+                CounterFeature()
+            }
+        )
+    }
+}
