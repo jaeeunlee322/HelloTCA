52e20745bb04a076272b4e4d9b9cd100d4dfa306
Jae-eun <jaeeun.lee@wesang.com>
2023-07-10 01:08:29 +0900

    두번째 프로젝트 추가, 폴더 구조 변경

---
+++ ContactsFeature/ContactsFeature/ContactsFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsFeatureApp.swift
+//  ContactsFeature
+//
+//  Created by 이재은 on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct ContactsFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
